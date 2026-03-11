45c1b6c5e95ad3e800cbd3fe8166d9187e37a265
Ariadna <ariadnaperez.dev@gmail.com>
2026-03-11 13:11:57 +0100

    event + event/id

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const eventController = require("../controllers/eventController");
 
-router.get("/", (req, res) => {
-res.json({ message: "Lista de eventos" });
-});
+router.get("/", eventController.getEvents);
+router.get("/:id", eventController.getEventById);
+router.post("/", eventController.createEvent);
+router.put("/:id", eventController.updateEvent);
+router.delete("/:id", eventController.deleteEvent);
 
 module.exports = router;
 
