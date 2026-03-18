d11d7920daa71297f042f460db5d8b20dc65dbc8
Ariadna <ariadnaperez.dev@gmail.com>
2026-03-18 14:45:54 +0100

    Resuelto conflicto

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,25 +1,31 @@
+/* Con un model:
+defines los campos
+defines el tipo de datos
+defines reglas (validaciones)
+conectas con la base de datos*/
+
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema(
-  {
+{
     title: {
-      type: String,
-      required: true,
+    type: String,
+    required: true,
     },
     description: {
-      type: String,
+    type: String,
     },
     date: {
-      type: Date,
-      required: true,
+    type: Date,
+    required: true,
     },
     location: {
-      type: String,
+    type: String,
     },
-  },
-  {
+},
+{
     timestamps: true,
-  }
+}
 );
 
 module.exports = mongoose.model("Event", eventSchema);
