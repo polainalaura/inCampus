841fdc0fe2f17b81c30d08d459fb6d1d381b9913
guillemmartin05 <guillemartinj@gmail.com>
2026-03-18 14:45:20 +0100

    cambiada config db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+
 const eventRoutes = require("./routes/eventRoutes");
 const subjectRoutes = require("./routes/subjectRoutes");
+const userRoutes = require("./routes/userRoutes"); // 👈 añadimos esto
+
+const db = require("./config/db"); // 👈 conexión MySQL
 
 const app = express();
 const PORT = 3000;
@@ -11,8 +15,12 @@ app.get("/", (req, res) => {
   res.send("API inCampus funcionando");
 });
 
+// rutas que ya tenías
 app.use("/events", eventRoutes);
 app.use("/subjects", subjectRoutes);
+
+// ruta nueva para probar MySQL
+app.use("/usuarios", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
