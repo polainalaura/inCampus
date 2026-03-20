d4ba93bdf697bf361df21036015e48c63fdc1b73
Ariadna <ariadnaperez.dev@gmail.com>
2026-03-20 12:55:47 +0100

    server y app separado

--- src/app.js
+++ src/app.js
@@ -6,10 +6,11 @@ const userRoutes = require("./routes/userRoutes");
 const tutoriaRoutes = require("./routes/tutorialRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-const db = require("./config/db");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+
+require("./config/db");
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
@@ -17,13 +18,12 @@ app.get("/", (req, res) => {
   res.send("API inCampus funcionando");
 });
 
-
 app.use("/auth", authRoutes);
 app.use("/events", eventRoutes);
 app.use("/subjects", subjectRoutes);
 app.use("/usuarios", userRoutes);
 app.use("/tutorias", tutoriaRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.use(errorHandler);
+
+module.exports = app;
